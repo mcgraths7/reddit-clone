c31729ddafe55d9c717dbc700e1282f53f953205
mcgraths7 <mcgrath.steven7@gmail.com>
2020-07-22 16:13:59 -0400

    Finished skeleton of users, added a form for creating and editing users, and all associated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show, :edit, :update, :destroy]
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
 end
