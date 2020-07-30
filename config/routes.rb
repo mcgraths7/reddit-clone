36d511f637b40e224cc346b4f375bcce9abf4e20
mcgraths7 <mcgrath.steven7@gmail.com>
2020-07-30 13:28:50 -0400

    One level nested comments added. Next step is to add unlimited nesting

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :edit, :update, :destroy]
   resources :topics, except: [:index]
   resources :posts, except: [:index] do
-    get 'comments/new' => 'comments#new'
+    get 'comments/new' => 'comments#new', as: 'new_comment'
   end
-  post 'comments' => 'comments#create'
+  resources :comments, only: [:create, :show, :edit, :destroy]
   get 'feed' => 'topics#index'
   get 'signup' => 'users#new'
   post 'signup' => 'users#create'
